547d1442c380f6375c2271220bf6c21867223414
PrachiDeore <163102472+PrachiDeore@users.noreply.github.com>
2026-01-12 20:00:51 +0530

    Add files via upload

---
+++ storyData.js
@@ -0,0 +1,9 @@
+export const characters = ["Princess", "Lion", "Robot", "Fairy"];
+export const places = ["Forest", "Castle", "Space", "Ocean"];
+
+export const endings = [
+  "They became best friends forever.",
+  "They found a hidden treasure.",
+  "They saved their world bravely.",
+  "They learned the power of kindness."
+];
